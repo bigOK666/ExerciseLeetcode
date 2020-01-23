9962031b147c536eb717166ef0f720ffd70067eb
Lu <zeren.lu@gmail.com>
2020-01-24 00:26:04 +0100

    considerd first character is right character

--- 20ValidParentheses.py
+++ 20ValidParentheses.py
@@ -6,11 +6,15 @@ class Solution:
         for item in s:
             if item in map.keys():
                 stack.append(item)
-            elif item in map.values():
+            elif item in map.values() and len(stack) != 0:
                 left_pair = list(map.keys())[list(map.values()).index(item)]
                 if stack[-1] == left_pair:
                     stack.pop()
                 else:
                     return False
+            elif item in map.values() and len(stack) == 0:
+                return False
         if len(stack) == 0:
             return True
+        else:
+            return False
