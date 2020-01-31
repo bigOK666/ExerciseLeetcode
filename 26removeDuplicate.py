f74f5b2af2a6b30b80e90bf54b3b9ad406e2d169
Lu <zeren.lu@gmail.com>
2020-01-31 22:37:35 +0100

    Update 26removeDuplicate.py
    倒序遍历，index为0的第一个元素不用作判断

--- 26removeDuplicate.py
+++ 26removeDuplicate.py
@@ -1,3 +1,8 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        for (i=0; i<len(nums);i++):
+        for item in range(len(nums)-1, -1, -1):
+            if item != 0 and nums[item] == nums[item - 1]:
+                nums.pop(item)
+        return len(nums)
+
+            
