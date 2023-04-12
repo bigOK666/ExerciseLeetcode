a522d4efd6dc284aa75cdbd1406fd436d03f8d1c
Lu <zeren.lu@gmail.com>
2023-04-12 23:37:40 +0200

    fixed size sorted array

--- DataStructure/Implementation/array/DynamicArray.hpp
+++ DataStructure/Implementation/array/DynamicArray.hpp
@@ -1,36 +1,43 @@
-//实现一个动态扩容的数组
-class DynamicArray{
-    public:
-    DynamicArray():m_capacity(8), m_size(0), m_data(new int[m_capacity]){}
+// 实现一个动态扩容的数组
+class DynamicArray
+{
+public:
+    DynamicArray() : m_capacity(8), m_size(0), m_data(new int[m_capacity]) {}
 
-    ~DynamicArray(){
+    ~DynamicArray()
+    {
         delete[] m_data;
     }
 
-    void push_back(int value){
-        if(m_size==m_capacity){
+    void push_back(int value)
+    {
+        if (m_size == m_capacity)
+        {
             int new_capacity = m_capacity * 2;
-            int* new_data = new int[new_capacity];
-            for(int i = 0; i<m_capacity;i++){
+            int *new_data = new int[new_capacity];
+            for (int i = 0; i < m_capacity; i++)
+            {
                 new_data[i] = m_data[i];
             }
             delete[] m_data;
             m_data = new_data;
             m_capacity = new_capacity;
         }
-        m_data[m_size++]=value;
+        m_data[m_size++] = value;
     }
 
-    int operator[](int index){
+    int operator[](int index)
+    {
         return m_data[index];
     }
 
-    int size() const {
+    int size() const
+    {
         return m_size;
     }
 
-    private:
+private:
     int m_capacity;
     int m_size;
-    int* m_data;
-}
+    int *m_data;
+};
--- DataStructure/Implementation/array/SortedArray.hpp
+++ DataStructure/Implementation/array/SortedArray.hpp
@@ -1,1 +1,93 @@
-//大小固定的有序数组，支持动态增删改，而且可以合并为一个数组
+// 大小固定的有序数组，支持动态增删改，而且可以合并为一个数组
+
+#include <iostream>
+#include <stdexcept>
+
+const int capacity = 8;
+
+class FixedSortedArray
+{
+private:
+    int m_size;
+    int m_data[capacity];
+    int findPosition(int value) const
+    {
+        for (int i = 0; i < m_size; ++i)
+        {
+            if (m_data[i] == value)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+public:
+    FixedSortedArray() : m_size(0) {}
+    ~FixedSortedArray() {}
+
+    void add(int value)
+    {
+        if (m_size >= capacity)
+        {
+            throw std::out_of_range("array is full");
+        }
+
+        int position = m_size;
+        for (int i = m_size - 1; i >= 0; --i)
+        {
+            if (m_data[i] > value)
+            {
+                m_data[i + 1] = m_data[i];
+                position = i;
+            }
+            else
+            {
+                break;
+            }
+        }
+        m_data[position] = value;
+        ++m_size;
+    }
+
+    void remove(int value)
+    {
+        int position = findPosition(value);
+        if (position < 0)
+        {
+            return;
+        }
+
+        for (int i = position; i < m_size - 1; ++i)
+        {
+            m_data[i] = m_data[i + 1];
+        }
+
+        --m_size;
+    }
+
+    void modify(int oldValue, int newValue)
+    {
+        int position = findPosition(oldValue);
+        if (position < 0)
+        {
+            throw std::invalid_argument("no such value");
+        }
+        remove(oldValue);
+        add(newValue);
+    }
+
+    int size() const
+    {
+        return m_size;
+    }
+
+    int operator[](int index) const
+    {
+        if (index < 0 || index >= m_size)
+        {
+            throw std::out_of_range("Index out of range");
+        }
+        return m_data[index];
+    }
+};
