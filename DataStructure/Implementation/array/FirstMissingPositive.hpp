419334eeb9f3ee95339628a94bfd3f355e85ce41
Lu <zeren.lu@gmail.com>
2023-04-14 21:09:48 +0200

    first missing positive

--- DataStructure/Implementation/array/FirstMissingPositive.hpp
+++ DataStructure/Implementation/array/FirstMissingPositive.hpp
@@ -1,3 +1,42 @@
 /*Given an unsorted integer array nums, return the smallest missing positive integer.
 
 You must implement an algorithm that runs in O(n) time and uses constant extra space.*/
+
+#include <vector>
+
+using namespace std;
+class Solution
+{
+public:
+    int firstMissingPositive(vector<int> &nums)
+    {
+        int length = nums.size();
+
+        for (int i = 0; i < length; i++)
+        {
+            // 当两个要交换的值相等时也不交换
+            while (nums[i] > 0 && nums[i] <= length && nums[i] != (i + 1) && nums[i] != nums[nums[i] - 1])
+            {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+
+        for (int i = 0; i < length; i++)
+        {
+            if (nums[i] != (i + 1))
+            {
+                return (i + 1);
+            }
+        }
+
+        return (length + 1);
+    }
+
+private:
+    void swap(vector<int> &nums, int i, int j)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+};
