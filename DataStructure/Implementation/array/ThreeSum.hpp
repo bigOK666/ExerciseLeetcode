1865c8df1f739a66fbbf16bf037cef793d83ae62
zeren <zeren.lu@gmail.com>
2023-04-14 13:27:04 +0200

    3sum

--- DataStructure/Implementation/array/ThreeSum.hpp
+++ DataStructure/Implementation/array/ThreeSum.hpp
@@ -2,3 +2,35 @@
 
 Notice that the solution set must not contain duplicate triplets.
 */
+#include <vector>
+#include<set>
+using namespace std;
+class Solution{
+    public:
+    vector<vector<int>> threeSum(vector<int> &nums){
+        int n=nums.size();
+        sort(nums.begin(), nums.end());
+
+        set<vector<int>> findings;
+        vector<vector<int>> output;
+        for(int i=0;i<n-2;i++){
+            int low=i+1, high=n-1;
+            while(low<high){
+                if(nums[i]+nums[low]+nums[high]<0){
+                    low++;
+                }else if(nums[i]+nums[low]+nums[high]){
+                    high--;
+                }else{
+                    findings.insert({nums[i], nums[low],nums[high]});
+                    low++;
+                    high--;
+                }
+            }
+        }
+
+        for(auto value:findings){
+            output.push_back(value);
+        }
+        return output;
+    }
+};
